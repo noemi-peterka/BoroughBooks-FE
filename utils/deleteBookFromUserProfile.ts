f7d4450c7ae94692f7c58781d2ce107af2746b19
J-Mro <j.k.mroueh@gmail.com>
2026-03-19 11:45:11 +0000

    refactor try block of deleteBookFromUserProfile with conditional logic to match endpoints from API

--- utils/deleteBookFromUserProfile.ts
+++ utils/deleteBookFromUserProfile.ts
@@ -6,8 +6,9 @@ export async function deleteBookFromUserProfile(
   collection: string,
 ): Promise<void | Object> {
   try {
+    const destination = collection === "library" ? "my-library" : "wish-list";
     const response = await axios.delete<void | Object>(
-      `https://boroughbooks.onrender.com/api/users/${username}/${collection}/${isbn}`,
+      `https://boroughbooks.onrender.com/api/users/${username}/${destination}/${isbn}`,
     );
     return;
   } catch (err) {
